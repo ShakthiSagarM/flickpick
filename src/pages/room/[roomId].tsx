491b0e24da5756865e2ffa3088ebe08d53976d0b
shakthi.m <shakthi.m@go-mmt.com>
2024-07-08 11:39:53 +0530

    bug fix

--- src/pages/room/[roomId].tsx
+++ src/pages/room/[roomId].tsx
@@ -12,7 +12,7 @@ const RoomId: React.FC<RoomIdProps> = () => {
 
     const {userName, setUserName} = useUser();
 
-    const [isUsernameSet, setIsUsernameSet] = React.useState<boolean>(userName);
+    const [isUsernameSet, setIsUsernameSet] = React.useState<boolean>(userName !== "");
 
     React.useEffect(() => {
         if (!isUsernameSet) {
